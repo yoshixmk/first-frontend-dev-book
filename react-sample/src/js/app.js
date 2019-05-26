95dbffaac98320a1a9611d3376c5a2971bd98719
YoshihiroUeki <pirori67@gmail.com>
2019-05-26 10:17:45 +0900

    react render

--- react-sample/src/js/app.js
+++ react-sample/src/js/app.js
@@ -1,3 +1,11 @@
-import hello from './hello'
+import hello from './hello';
+import React from 'react';
+import ReactDom from 'react-dom';
+import style from '../css/style';
 
 hello()
+
+ReactDom.render(
+  <h1>Hello, Frontend Engneer!</h1>,
+  document.getElementById('root')
+)
