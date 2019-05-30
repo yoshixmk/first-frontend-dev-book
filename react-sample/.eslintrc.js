40ae84f771a4aa09a7fe6e40181a59d3b8c99908
YoshihiroUeki <pirori67@gmail.com>
2019-05-30 11:10:18 +0900

    add eslint

---
+++ react-sample/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "brawser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "constructor-super": "warn",
+        "no-const-assign": "warn",
+        "no-this-before-super": "warn",
+        "no-undef": "warn",
+        "no-unreachable": "warn",
+        "no-unused-vars": "warn",
+        "valid-typeof": "warn"
+    }
+};
