7d78d2022d5c3705acc1e30912c1e596102e748b
YoshihiroUeki <pirori67@gmail.com>
2019-06-01 15:46:31 +0900

    user request check for token auth

--- chat-server/functions/index.js
+++ chat-server/functions/index.js
@@ -9,6 +9,33 @@ const app = express()
 const cors = require('cors')({origin: true})
 app.use(cors)
 
+const anonymousUser = {
+  id: "anon",
+  name: "Anonymous",
+  avator: ""
+}
+
+const checkUser = (req, res, next) => {
+  req.user = anonymousUser
+  if (req.query.auth != undefined) {
+    const idToken = req.query.auth_token
+    admin.auth().token.verifyIdToken(idToken)
+      .then(decodedIdToken => {
+        const authUser = {
+          id: docodedIdToken.user_id,
+          name: decodedIdToken.name,
+          avator: decodedIdToken.user.picture
+        }
+        req.user = authUser
+        next()
+      })
+      .catch(error => {next()})
+  }
+  next()
+}
+
+app.use(checkUser)
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
