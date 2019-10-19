af38de6afb983dd0aa76f68a1fb928722f2c7337
YoshihiroUeki <pirori67@gmail.com>
2019-10-19 14:48:53 +0900

    MessageFeed render

--- react-sample-app/src/components/MessageFeed.tsx
+++ react-sample-app/src/components/MessageFeed.tsx
@@ -9,3 +9,36 @@ interface MessageFeedProps {
 interface MessageFeedState {
   messages: Message[];
 }
+
+export class MessageFeed extends React.Component<MessageFeedProps, MessageFeedState> {
+  constructor(props: MessageFeedProps) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
+
+  public render() {
+    return (
+      <Comment.Group>
+        <Header as='h3' dividing>{this.props.channelName}</Header>
+        {this.state.messages.slice().reverse().map(message => {
+            return (
+              <Comment key={message.id}>
+                <Comment.Avatar src={message.user.avatar || '/img/avatar.png'} />
+                <Comment.Content>
+                  <Comment.Author as='a'>@{message.user.name}</Comment.Author>
+                  <Comment.Metadata>
+                    <div>{message.date}</div>
+                  </Comment.Metadata>
+                  <Comment.Text>
+                    {message.body}
+                  </Comment.Text>
+                </Comment.Content>
+              </Comment>
+            );
+        })}
+      </Comment.Group>
+  );
+  }
+}
