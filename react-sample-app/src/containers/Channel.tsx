72fc71c822c2f112ae9269512e87c6caacb76063
YoshihiroUeki <pirori67@gmail.com>
2019-10-20 23:19:08 +0900

    MessageFeed in Channel

--- react-sample-app/src/containers/Channel.tsx
+++ react-sample-app/src/containers/Channel.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
+import {match} from 'react-router-dom';
+import {MessageFeed} from '../components';
 
-export const Channel = () => {
-  return (
-    <></>
-  );
+interface ChannelMatch {
+  channelName: string;
+}
+
+interface ChannelProps {
+  match: match<ChannelMatch>;
+}
+
+export interface match<P> {
+  params: P;
+  isExact: boolean;
+  path: string;
+  url: string;
+}
+
+export class Channel extends React.Component<ChannelProps, {}> {
+  constructor(props: ChannelProps) {
+    super(props);
+  }
+  public render() {
+    const {channelName} = this.props.match.params;
+    return (
+      <MessageFeed channelName={channelName} />
+    );
+  }
 };
