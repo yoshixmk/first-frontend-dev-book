23fdfd5f269faab95d7205894ef0880b10802065
YoshihiroUeki <pirori67@gmail.com>
2019-11-28 23:58:10 +0900

    pug str

--- vue-chat/src/App.vue
+++ vue-chat/src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    router-view
 </template>
 
 <script>
