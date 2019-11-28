59daaedcbddea4e52e2b6f365d4fb79b125f20f7
YoshihiroUeki <pirori67@gmail.com>
2019-11-29 00:04:33 +0900

    wip: App vue using pug

--- vue-chat/src/App.vue
+++ vue-chat/src/App.vue
@@ -1,22 +1,20 @@
-<template lang="pug">
-  #app
-    img(src="./assets/logo.png")
-    router-view
-</template>
-
+<template lang="pug" src="./app.pug" />
 <script>
 export default {
-  name: 'App'
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    /*-webkit-font-smoothing: antialiased;*/
+    /*-moz-osx-font-smoothing: grayscale;*/
+    /*text-align: center;*/
+    /*color: var(--color);*/
+    /*margin-top: 60px;*/
+  }
 </style>
--- vue-chat/src/main.js
+++ vue-chat/src/main.js
@@ -1,8 +1,15 @@
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/ja'
+
+Vue.use(ElementUI, {locale})
 
 Vue.config.productionTip = false
 
@@ -10,6 +17,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: { App }
 })
