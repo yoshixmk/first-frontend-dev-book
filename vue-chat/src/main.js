cd69a9c10c5e69ef34d5570db19ec23c3c27c23a
YoshihiroUeki <pirori67@gmail.com>
2019-11-30 22:19:34 +0900

    npm i . mv chat file

--- vue-chat/src/main.js
+++ vue-chat/src/main.js
@@ -1,10 +1,7 @@
-
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store';
+// import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/ja'
@@ -17,7 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  store,
+  // store,
   template: '<App/>',
   components: { App }
 })
