c66a6265765ef3c86809a6c95b013470dca95e13
YoshihiroUeki <pirori67@gmail.com>
2019-12-04 22:26:50 +0900

    add component

--- ng-chat/src/app/app.module.ts
+++ ng-chat/src/app/app.module.ts
@@ -1,21 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import {MatToolbarModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+// import { ChannelComponent } from './channel/channel.component';
+// import { MessageFeedComponent } from './message-feed/message-feed.component';
+// import { MessageFormComponent } from './message-form/message-form.component';
+
+// import { ReversePipe } from './pipes/reverse.pipe';
+
+// import { MessageService } from './services/message.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ChannelComponent,
+    // MessageFeedComponent,
+    // MessageFormComponent,
+    // ReversePipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
-    MatToolbarModule
+
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
-  providers: [],
+  providers: [/*MessageService*/],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
