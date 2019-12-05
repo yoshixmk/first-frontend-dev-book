ff58d582aa1c28e27559b09bcb70e0b2a4f994d2
YoshihiroUeki <pirori67@gmail.com>
2019-12-06 00:29:02 +0900

    active store

--- react-test/src/js/hello.js
+++ react-test/src/js/hello.js
@@ -1,3 +1,4 @@
 export default function() {
+    // eslint-disable-next-line no-console
     console.log('Hello Frontend Engineer!')
 }
--- vue-chat/src/main.js
+++ vue-chat/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import store from './store'
+import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/ja'
@@ -14,7 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  // store,
+  store,
   template: '<App/>',
   components: { App }
 })
